b850d740d18c2fd4c7e744b9c0daab91fe32fa9f
zaidk2021 <kzaidnba@gmail.com>
2024-01-07 16:58:56 +0530

    Track .env file

--- foodrecommender/app.py
+++ foodrecommender/app.py
@@ -175,8 +175,8 @@ import streamlit as st
 # Access MONGODB_URI
 #mongodb_uri = os.environ.get("MONGODB_URI")
 
-#import os
-#import streamlit as st
+import os
+import streamlit as st
 
 # Access the database connection string from Streamlit's secrets
 db_conn_string = st.secrets["DB_CON_STRING"]
